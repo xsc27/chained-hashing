8ed3e202a7429f9974ecf8922deb15bcf68563f2
Carlos Meza <carlos@digitalr00ts.com>
2021-12-13 12:20:44 -0800

    docs(gen_stubs): fix navigation and add edit link

--- docs/_scripts/gen_stubs.py
+++ docs/_scripts/gen_stubs.py
@@ -8,8 +8,9 @@ for path in sorted(Path("src").glob("**/*.py")):
     doc_path = path.relative_to("src", "chained_hashing").with_suffix(".md")
     NAV[doc_path.with_suffix("").parts] = doc_path
 
+    mkdocs_gen_files.set_edit_path(Path("reference", doc_path), path)
     with mkdocs_gen_files.open(Path("reference", doc_path), "w") as fid:
         print(f"::: {'.'.join(path.relative_to('src').with_suffix('').parts)}", file=fid)
 
-with mkdocs_gen_files.open("reference/index.md", "w") as nav_file:
-    nav_file.writelines(NAV.build_literate_nav())
+# with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+#     nav_file.writelines(NAV.build_literate_nav())
